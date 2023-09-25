cbf937fb57be38f8cd5be9abdce8098dfb7ae51c
Oleksii <rybalko339@gmail.com>
2023-09-25 17:21:25 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+
+
+public class Main {
+    static void calc(double numA, int numB, char operation) {
+        double result = 0;
+        switch (operation) {
+            case '+'
+                operation == '+';
+            {
+                result = numA + numB;
+                break;
+            }
+            case '-'
+                operation == '-';
+            {
+                result = numA - numB;
+                break;
+            }
+            case '*'
+                operation == '*';
+            {
+                result = numA * numB;
+                break;
+            }
+            case '/'
+                operation == '/';
+            {
+                result = numA / numB;
+                break;
+            }
+            default:
+                System.out.println("Oops, it's incorrect operation");
+        }
+    }
+}
+
